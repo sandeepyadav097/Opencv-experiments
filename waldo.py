7720d39548ec562a6eadd317d76ece22055a60d2
sandeepyadav097 <sandeepyadav097@gmail.com>
2017-12-08 09:13:31 +0530

    Template matching done

---
+++ waldo.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+
+image=cv2.imread('WaldoBeach.jpg')
+cv2.imshow('waldo', image)
+cv2.waitKey(0)
+
+gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+template=cv2.imread('WaldoBeach.jpeg', 0)
+cv2.imshow('template', template)
+cv2.waitKey(0)
+result=cv2.matchTemplate(gray,template,cv2.TM_CCOEFF)
+
+min_val, max_val, min_loc,max_loc=cv2.minMaxLoc(result)
+
+top_left=max_loc
+bottom_right=(top_left[0]+50, top_left[1]+50)
+cv2.rectangle(image, top_left, bottom_right, (0,0,255), 5)
+cv2.imshow('image', image)
+
+
+cv2.waitKey(0)
